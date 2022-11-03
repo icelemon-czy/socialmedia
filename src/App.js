bbd6afc6c0ab6f0c2916008854fc70275966d012
icelemon-czy <71572625+icelemon-czy@users.noreply.github.com>
2022-11-03 12:42:31 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+} from "react-router-dom";
 
 function App() {
+    const Layout = ()=>{
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+    /* Important !!!*/
+    const router = createBrowserRouter([
+        {
+            path:"/login",
+            element: <Login/>
+        },
+        {
+            path:"/register",
+            element: <Register/>
+        },
+    ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <RouterProvider router={router} />
+      </div>
   );
 }
 
---
+++ src/components/leftbar/leftbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./leftbar.scss"
+
+const Leftbar =() => {
+    return (
+        <div className={"leftbar"}> LeftBar </div>
+    )
+}
+
+export default Leftbar;
