6bace8081646343507dec096306b4ce2b25a9a5d
icelemon-czy <71572625+icelemon-czy@users.noreply.github.com>
2022-11-12 23:42:12 -0500

    update

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,5 +1,8 @@
 import React, {useContext} from "react"
 import "./share.scss"
+import Image from "../../images/img.png";
+import Map from "../../images/map.png";
+import Friend from "../../images/friend.png";
 import { AuthContext } from "../../context/authContext";
 
 const Share = ()=>{
@@ -9,19 +12,35 @@ const Share = ()=>{
             <div className={"container"}>
                 <div className={"top"}>
                     {/*Picture + text */}
-                    <div className={"left"}>
-                        <img src={"/upload"+currentUser.profilePic} alt={""} />
-                        <input
-                            type={"text"}
-                            placeholder={`What's on your mind ${currentUser.name} ?`}
-                        />
-
-
-                    </div>
+                    <img src={currentUser.profilePic} alt={""}/>
+                    <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} />
                 </div>
                 <hr />
+                <div className={"bottom"}>
+                    <div className={"left"}>
+                        <input type="file" id="file" style={{display:"none"}} />
+                        <label htmlFor={"file"}>
+                            <div className="item">
+                                <img src={Image} alt={""} />
+                                <span>Add Image</span>
+                            </div>
+                        </label>
 
+                        <div className="item">
+                            <img src={Map} alt={""} />
+                            <span>Add Place</span>
+                        </div>
 
+                        <div className="item">
+                            <img src={Friend} alt={""} />
+                            <span>Tag Friends</span>
+                        </div>
+
+                    </div>
+                    <div className={"right"}>
+                        <button>Share</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
